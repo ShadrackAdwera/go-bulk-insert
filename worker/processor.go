6efcd3c2975bf8f4dfd03b567a23f8b45ed9b36e
ShadrackAdwera <adweshshaddie@gmail.com>
2023-05-25 08:54:05 +0300

    add start fn to processor struct

--- worker/processor.go
+++ worker/processor.go
@@ -10,14 +10,15 @@ import (
 
 type TaskProcessor interface {
 	TaskProcessData(ctx context.Context, task *asynq.Task) error
+	Start() error
 }
 
 type DataTaskProcessor struct {
 	server *asynq.Server
-	store  db.Store
+	store  db.TxStore
 }
 
-func NewTaskServer(opts asynq.RedisClientOpt, store db.Store) TaskProcessor {
+func NewTaskServer(opts asynq.RedisClientOpt, store db.TxStore) TaskProcessor {
 	server := asynq.NewServer(opts, asynq.Config{
 		ErrorHandler: asynq.ErrorHandlerFunc(func(ctx context.Context, task *asynq.Task, err error) {
 			log.Err(err).Str("task_type", task.Type()).Bytes("payload", task.Payload()).Msg("error processing task . . ")
@@ -26,4 +27,12 @@ func NewTaskServer(opts asynq.RedisClientOpt, store db.Store) TaskProcessor {
 	return &DataTaskProcessor{
 		server, store,
 	}
+}
+
+func (processor *DataTaskProcessor) Start() error {
+	mux := asynq.NewServeMux()
+
+	mux.HandleFunc(TaskInsertData, processor.TaskProcessData)
+
+	return processor.server.Start(mux)
 }
