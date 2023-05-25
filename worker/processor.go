737f5b7eba55c5228df0feacdeae5534cfc0d622
ShadrackAdwera <adweshshaddie@gmail.com>
2023-05-25 08:42:35 +0300

    set up task processor

---
+++ worker/processor.go
@@ -0,0 +1,29 @@
+package worker
+
+import (
+	"context"
+
+	db "github.com/ShadrackAdwera/go-bulk-insert/db/sqlc"
+	"github.com/hibiken/asynq"
+	"github.com/rs/zerolog/log"
+)
+
+type TaskProcessor interface {
+	TaskProcessData(ctx context.Context, task *asynq.Task) error
+}
+
+type DataTaskProcessor struct {
+	server *asynq.Server
+	store  db.Store
+}
+
+func NewTaskServer(opts asynq.RedisClientOpt, store db.Store) TaskProcessor {
+	server := asynq.NewServer(opts, asynq.Config{
+		ErrorHandler: asynq.ErrorHandlerFunc(func(ctx context.Context, task *asynq.Task, err error) {
+			log.Err(err).Str("task_type", task.Type()).Bytes("payload", task.Payload()).Msg("error processing task . . ")
+		}),
+	})
+	return &DataTaskProcessor{
+		server, store,
+	}
+}
